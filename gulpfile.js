1d0fda7333acc653e5d8e18ecce56443b603b777
@rgem52 <rgomez@hi-iberia.es>
2015-06-01 10:46:34 +0200

    Deleting Files in a Dependency Task Creating a Watch Task to Compile CSS Handling Errors and Using Gulp Plumber

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@
  */
 var gulp = require('gulp');
 var args = require('yargs').argv;
-var $ = require('gulp-load-plugins')({lazy: true});
 var config = require('./gulp.config')();
+var del = require('del');
+
+var $ = require('gulp-load-plugins')({lazy: true});
 
 //var jshint = require('gulp-jshint');
 //var jscs = require('gulp-jscs');
@@ -23,11 +25,38 @@ gulp.task('vet', function() {
         .pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', ['clean-styles'] , function() {
     console.log('Compiling Less ---> CSS');
     return gulp
         .src(config.less)
+        .pipe($.plumber())
         .pipe($.less())
+        //.on('error',errorLogger())
         .pipe($.autoprefixer({browsers:['last 2 versions', '> 5%']}))
         .pipe(gulp.dest(config.temp));
 });
+
+gulp.task('clean-styles', function(done) {
+    console.log('Cleaning files');
+    var files = config.temp + '**/*.css';
+    clean(files,done);
+
+});
+
+gulp.task('less-watcher',function(){
+    gulp.watch([config.less],['styles']);
+});
+
+
+///////////
+//function errorLogger(error){
+//    console.log('****** Start of Error ******');
+//    console.log(error);
+//    console.log('****** End of Error ******');
+//    this.emit('end');
+//}
+
+function clean(path,done) {
+    console.log('Cleaning files: ' + path);
+    del(path,done);
+}
